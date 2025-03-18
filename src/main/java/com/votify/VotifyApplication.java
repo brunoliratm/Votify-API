404059b2ee34d06af7ddf5ba801bf2184f5673b6
brunoliratm <bmagnoserver@gmail.com>
2025-03-17 23:55:19 -0300

    Initialize Votify application with basic structure and configuration

---
+++ src/main/java/com/votify/VotifyApplication.java
@@ -0,0 +1,13 @@
+package com.votify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotifyApplication.class, args);
+	}
+
+}
